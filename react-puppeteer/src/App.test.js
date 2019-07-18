c45d9b6c12258bc0a49597d04cb7777ccf65eb7a
Catalin Avram <papiot@gmail.com>
2019-07-17 22:05:13 -0400

    Correct merge

--- react-puppeteer/src/App.test.js
+++ react-puppeteer/src/App.test.js
@@ -28,7 +28,7 @@ describe('H1 Text', () => {
 	await page.waitForSelector('.App-link');
 
 	const html = await page.$eval('.App-link', e => e.innerHTML);
-	expect(html).toBe("Learn Reactaaa");
+	expect(html).toBe("Learn React");
 
 	browser.close();
   }, 16000);
